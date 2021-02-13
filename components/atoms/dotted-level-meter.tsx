715b82bf20f66e1fcde74b4d8183aebdd8bf4b4f
Haruki Nazawa <nazawaharuki@gmail.com>
2021-02-14 08:36:25 +0900

    DottedLevelMeterを実装

---
+++ components/atoms/dotted-level-meter.tsx
@@ -0,0 +1,33 @@
+import React from "react"
+
+type DottedLevelMeterProps = {
+  className: string;
+  max: number;
+  min: number;
+  level: number;
+  width: number;
+  dotsCount: number;
+}
+
+export default function DottedLevelMeter(props: Partial<DottedLevelMeterProps>) {
+  props.max ??= 1;
+  props.min ??= 0;
+  props.width ??= 120;
+  props.dotsCount ??= 6;
+  props.level ??= 0;
+
+  const validDotsCount = Math.floor(props.level / (props.max - props.min) * props.dotsCount);
+
+  const dots: React.ReactNode[] = []
+  for (let n=1; n<props.dotsCount; n++) {
+    dots.push(
+      <div className={`w-4 ${validDotsCount >= n ? "bg-green-500" : "bg-white"} w-3`} />
+    );
+  }
+
+  return (
+    <div className={`flex justify-between ${props.className}`}>
+      {dots}
+    </div>
+  );
+}
--- pages/sampler/sample.tsx
+++ pages/sampler/sample.tsx
@@ -3,7 +3,12 @@ import React from "react"
 export default function Sample() {
   return (
     <body className="bg-secondary">
-      <div>AbC</div>
+      <div id="sampler-sample" className="m-10">
+        <div className="flex justify-between ...">
+          <div>1</div>
+          <div>2</div>
+        </div>
+      </div>
     </body>
   )
 }
