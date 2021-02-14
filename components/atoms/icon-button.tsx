1568fc2647e17d87659f915b1272212a5d88afab
Haruki Nazawa <nazawaharuki@gmail.com>
2021-02-15 08:03:24 +0900

    ボタンサイズを小さくした

--- components/atoms/icon-button.tsx
+++ components/atoms/icon-button.tsx
@@ -9,7 +9,7 @@ export type SquareButtonProps = {
 export default function SquareButton(props: SquareButtonProps) {
   return (
     <button 
-      className={`${props.className} w-14 h-14 rounded-md shadow-md focus:outline-none`}
+      className={`${props.className} w-12 h-12 rounded-md shadow-md focus:outline-none`}
       onClick={props.onClick}
     >
       {props.children}
