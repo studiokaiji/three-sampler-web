96c4c68571c2924600b7c9540a18b42db2ba8f3b
Haruki Nazawa <nazawaharuki@gmail.com>
2021-02-14 23:05:03 +0900

    propsにボタンクリックのハンドラを指定させるようにした

--- components/molecules/mic-and-monitor-switcher.tsx
+++ components/molecules/mic-and-monitor-switcher.tsx
@@ -2,19 +2,23 @@ import React from "react";
 import IconButton from "../atoms/icon-button";
 import DottedLevelMeter, { DottedLevelMeterProps } from "../atoms/dotted-level-meter";
 
-export default function MicAndMonitorSwitcher(props: DottedLevelMeterProps) {
+export type MicAndMonitorSwitcherProps = {
+  onClickMicIcon: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+  onClickMonitorIcon: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+} & Partial<DottedLevelMeterProps>;
 
+export default function MicAndMonitorSwitcher(props: MicAndMonitorSwitcherProps) {
   return (
     <div className={props.className}>
-      <div className="flex justify-between">
-        <IconButton>
+      <DottedLevelMeter {...props} />
+      <div className="flex justify-between mt-3">
+        <IconButton className="bg-white" onClick={props.onClickMicIcon}>
           <img id="mic-icon" />
         </IconButton>
-        <IconButton>
+        <IconButton className="bg-white" onClick={props.onClickMonitorIcon}>
           <img id="headphone-icon" />
         </IconButton>
       </div>
-      <DottedLevelMeter {...props} className="w-32" />
     </div>
   );
 }
