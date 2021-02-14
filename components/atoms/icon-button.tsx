deb4378b75db375f803dcc080704bd8d1eeb78ab
Haruki Nazawa <nazawaharuki@gmail.com>
2021-02-14 09:07:03 +0900

    onClickを実装

--- components/atoms/icon-button.tsx
+++ components/atoms/icon-button.tsx
@@ -3,8 +3,16 @@ import React from "react"
 export type SquareButtonProps = {
   className?: string;
   children: React.ReactNode;
+  onClick: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
 }
 
 export default function SquareButton(props: SquareButtonProps) {
-  return <button className={`${props.className} w-14 h-14 rounded-md shadow-md`}>{props.children}</button>
+  return (
+    <button 
+      className={`${props.className} w-14 h-14 rounded-md shadow-md focus:outline-none`}
+      onClick={props.onClick}
+    >
+      {props.children}
+    </button>
+  )
 }
