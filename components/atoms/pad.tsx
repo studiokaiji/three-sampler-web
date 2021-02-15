bb809d0fdbb8e0c18615351f2b9edcdf45fe159c
Haruki Nazawa <nazawaharuki@gmail.com>
2021-02-16 07:24:37 +0900

    padを長押しに対応させた

--- components/atoms/pad.tsx
+++ components/atoms/pad.tsx
@@ -4,13 +4,21 @@ export type PadProps = {
   className?: string;
   children: React.ReactNode;
   onClick: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+  onHoldStart: () => void;
+  onHoldEnd: () => void;
 }
 
 export default function Pad(props: PadProps) {
+  const mouseDowned = () => {
+    setTimeout(props.onHoldStart, 200);
+  }
+
   return (
     <button
       className={`w-24 h-24 focus:outline-none ${props.className}`}
       onClick={props.onClick}
+      onMouseDown={mouseDowned}
+      onMouseUp={props.onHoldEnd}
     >
       {props.children}
     </button>
