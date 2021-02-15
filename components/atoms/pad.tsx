f7635ba26c0d2cf138a38a466eb60b51a32fc787
Haruki Nazawa <nazawaharuki@gmail.com>
2021-02-16 03:24:10 +0900

    pad componentを作成

---
+++ components/atoms/pad.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export type PadProps = {
+  className?: string;
+  children: React.ReactNode;
+  onClick: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+}
+
+export default function Pad(props: PadProps) {
+  return (
+    <button
+      className={`w-24 h-24 ${props.className}`}
+      onClick={props.onClick}
+    >
+      {props.children}
+    </button>
+  );
+}
