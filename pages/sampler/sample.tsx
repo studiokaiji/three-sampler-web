e41b7ae71a4e017dde22a8aa2c0c0c2253578688
Haruki Nazawa <nazawaharuki@gmail.com>
2021-02-16 13:16:10 +0900

    クリックで録音、再生ができるようにした

--- pages/sampler/sample.tsx
+++ pages/sampler/sample.tsx
@@ -1,11 +1,82 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import MicAndMonitorSwitcher from "../../components/molecules/mic-and-monitor-switcher";
 import SectionTab from "../../components/molecules/section-tab";
 import Router from "next/router";
 import Bank from "../../components/molecules/bank";
 import PadGrid from "../../components/molecules/pad-grid";
 
+const SampleContext = React.createContext<HTMLAudioElement[]>(Array(16));
+
+async function createStream() {
+  const stream = await navigator.mediaDevices.getUserMedia({
+    audio: true,
+    video: false,
+  });
+
+  return stream;
+} 
+
+let recorder: MediaRecorder;
+
 export default function Sample() {
+  const samples = useContext(SampleContext);
+
+  useEffect(() => {
+    createStream().then((stream) => {
+      recorder = new MediaRecorder(stream, {
+        mimeType: "audio/webm",
+        audioBitsPerSecond: 24 * 1000
+      });
+    }) 
+  }, []);
+
+  const startRecord = async (padIndex: number) => {
+    if (padIndex > 15 || padIndex < 0) throw Error("Invalid pad index.");
+
+    setTimeout(() => {
+      stopRecord(padIndex);
+    }, 5 * 60 * 1000);
+
+    recorder.start();
+
+    console.log("STARTTED")
+  }
+
+  const stopRecord = async (padIndex: number) => {
+    if (padIndex > 15 || padIndex < 0) throw Error("Invalid pad index.");
+    if (!recorder || recorder.state === "inactive") return;
+
+    recorder.addEventListener("dataavailable", (e) => {
+      const url = URL.createObjectURL(e.data);
+      samples[padIndex] = new Audio(url);
+
+      console.log("dataAvailable", url);
+    });
+
+    recorder.stop();
+  }
+
+  const playSample = (padIndex: number) => {
+    samples[padIndex].currentTime = 0;
+    samples[padIndex].play();
+    console.log("STARTED PLAY")
+  }
+
+  const stopSample = (padIndex: number) => {
+    samples[padIndex].pause();
+    console.log("STOPPED PLAY")
+    samples[padIndex].currentTime = 0;
+  }
+
+  const pressedPadHandler = (index: number) => {
+    if (samples[index]) playSample(index);
+    else startRecord(index);
+  }
+
+  const pressedEndPadHandler = (index: number) => {
+    if (!samples[index]) stopRecord(index);
+  }
+
   return (
     <body className="bg-secondary">
       <div id="sampler-sample" className="m-8 max-w-6xl grid gap-y-10">
@@ -33,7 +104,8 @@ export default function Sample() {
             className="w-9/12"
             selectedPadClassName="bg-primary"
             padIndex={0}
-            onSelectPad={(i) => console.log(i)}
+            onPressPad={(i) => pressedPadHandler(i)}
+            onPressEndPad={(i) => pressedEndPadHandler(i)}
           />
         </div>
       </div>
