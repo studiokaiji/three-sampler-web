8c7b4709a2aa4efa1d0b56594fc833f3ab78f9c3
Haruki Nazawa <nazawaharuki@gmail.com>
2021-02-17 07:11:02 +0900

    PadButtonにclassNameを読ませるようにした

--- components/atoms/pad.tsx
+++ components/atoms/pad.tsx
@@ -1,20 +1,49 @@
 import React from "react";
+import { useDropzone } from "react-dropzone";
 
 export type PadProps = {
   className?: string;
+  fileDraggable: boolean;
   children: React.ReactNode;
+  onDropFile: (file: any) => void;
   onMouseDown: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
   onMouseUp: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void
 }
 
-export default function Pad(props: PadProps) {
+const PadButton = (props: PadProps) => {
   return (
     <button
-      className={`w-24 h-24 focus:outline-none ${props.className}`}
+      className={`w-full h-full focus:outline-none ${props.className}`}
       onMouseDown={props.onMouseDown}
       onMouseUp={props.onMouseUp}
     >
       {props.children}
     </button>
+  );
+}
+
+export default function Pad(props: PadProps) {
+  if (!props.fileDraggable) {
+    return (
+      <div className="w-24 h-24">
+        <PadButton {...props} />
+      </div>
+    );
+  }
+
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({
+    onDrop: () => props.onDropFile,
+    noClick: true,
+  });
+
+  return (
+    <div {...getRootProps({ className: `w-24 h-24 bg-white ${props.className}` })}>
+      <input {...getInputProps()} className="opacity-0" />
+      {
+        isDragActive ? 
+          <p className="w-full h-full flex justify-center items-center">Add File</p> :
+          <PadButton {...props} />
+      }
+    </div>
   );
 }
