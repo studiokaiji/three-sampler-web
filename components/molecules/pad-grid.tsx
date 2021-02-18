df29816df3fc3fb01ae39c3c9299a9776f56bb42
Haruki Nazawa <nazawaharuki@gmail.com>
2021-02-19 06:20:51 +0900

    waveformを表示するようにした

--- components/molecules/pad-grid.tsx
+++ components/molecules/pad-grid.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Pad from "../atoms/pad";
+import WaveForm from "../atoms/waveform";
 
 const PAD_COUNT = 16;
 
@@ -8,6 +9,8 @@ export type PadGridProps = {
   selectedPadClassName: string;
   unselectedPadClassName?: string;
   padIndex: number;
+  sampleUrls: string[];
+  onDropFile: (file: File, index: number) => void;
   onPressPad: (index: number) => void;
   onPressEndPad: (index: number) => void;
 }
@@ -19,17 +22,21 @@ export default function PadGrid(props: PadGridProps) {
   for (let i=0; i<PAD_COUNT; i++) {
     pads.push(
       <Pad
-        className={props.padIndex === i ? props.selectedPadClassName : unselectedPadClassName}
+        className={(props.padIndex === i ? props.selectedPadClassName : unselectedPadClassName) + " w-24 h-24"}
+        key={`sample-pad-${i}`}
+        onDropFile={(file) => props.onDropFile(file, i)}
         onMouseDown={() => props.onPressPad(i)}
         onMouseUp={() => props.onPressEndPad(i)}
       >
-        <p>{i}</p>
+        {
+          props.sampleUrls[i] && <WaveForm url={props.sampleUrls[i]} peakLength={60} className="w-24 h-24" />
+        }
       </Pad>
     );
   }
 
   return (
-    <div className="grid grid-cols-8 gap-3">
+    <div id="samplepad-grid" className="grid grid-cols-8 gap-3">
       {pads}
     </div>
   );
