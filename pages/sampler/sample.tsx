5c40395bb185483970b4570f03ef0905e33e70a0
Haruki Nazawa <nazawaharuki@gmail.com>
2021-02-19 06:21:54 +0900

    sample ClassにoneShotpプロパティを追加し、waveformを削除した。

--- pages/sampler/sample.tsx
+++ pages/sampler/sample.tsx
@@ -4,30 +4,29 @@ import SectionTab from "../../components/molecules/section-tab";
 import Router from "next/router";
 import Bank from "../../components/molecules/bank";
 import PadGrid from "../../components/molecules/pad-grid";
-import WaveForm from "../../components/atoms/waveform";
 
 class Sample {
   audio: HTMLAudioElement;
   pitch: number;
   volume: number;
   pan: number;
-  waveform: typeof WaveForm | null;
+  oneShot: boolean;
 
-  constructor(opts: {
-    audio?: HTMLAudioElement;
-    pitch?: number;
-    volume?: number;
-    pan?: number;
-    waveform?: typeof WaveForm;
-  }) {
+  constructor(opts?: Partial<{
+    audio: HTMLAudioElement;
+    pitch: number;
+    volume: number;
+    pan: number;
+    oneShot: boolean;
+  }>) {
+    opts ??= {};
     this.audio = opts.audio || new Audio();
     this.pitch = opts.pitch || 0;
-    this.volume = opts.volume || 0;
+    this.volume = opts.volume || 1;
     this.pan = opts.pan || 0;
-    this.waveform = opts.waveform || null;
+    this.oneShot = opts.oneShot || false;
   }
 }
-
 
 type SampleAction =
 {
@@ -42,9 +41,11 @@ type SampleAction =
 function samplesReducer(state: Sample[], action: SampleAction) {
   if (action.type === "assign") {
     state[action.index] = action.value;
-    console.log(state);
+    state.forEach((sample) => {
+      console.log(sample.audio);
+    });
   } else {
-    state[action.index] = null;
+    state[action.index] = new Sample();
   }
 
   return state;
@@ -92,16 +93,17 @@ export default function SamplePage() {
     recorder.addEventListener("dataavailable", (e) => {
       const url = URL.createObjectURL(e.data);
       loadSample(url, padIndex);
-    });
+    }, { once: true });
 
     recorder.stop();
   }
 
   const loadSample = (url: string, padIndex: number) => {
+    console.log("LOAD SAMPLE", padIndex);
     dispatchSamples({
       type: "assign",
       index: padIndex,
-      value: new Audio(url)
+      value: new Sample({ audio: new Audio(url) })
     });
 
     const urls = sampleUrls.concat();
@@ -111,31 +113,32 @@ export default function SamplePage() {
   }
 
   const playSample = (padIndex: number) => {
-    samples[padIndex].currentTime = 0;
-    samples[padIndex].play();
+    samples[padIndex].audio.currentTime = 0;
+    samples[padIndex].audio.play();
   }
 
   const stopSample = (padIndex: number) => {
-    if (samples[padIndex]) {
-      samples[padIndex].pause();
-      samples[padIndex].currentTime = 0; 
-    }
+    samples[padIndex].audio.pause();
+    samples[padIndex].audio.currentTime = 0; 
   }
 
   const stopAllSample = () => {
     for (let i=0; i<samples.length; i++) {
-      stopSample(i);
+       if (samples[i]) stopSample(i);
     }
   }
 
   const pressedPadHandler = (index: number) => {
+    console.log(samples[index]);
     if (samples[index]) playSample(index);
     else startRecord(index);
     setPadIndex(index);
   }
 
   const pressedEndPadHandler = (index: number) => {
-    if (!samples[index]) stopRecord(index);
+    const sample = samples[index];
+    if (!sample) stopRecord(index);
+    else if (!sample.oneShot) stopSample(index);
   }
 
   const droppedFileHandler = (file: File, index: number) => {
