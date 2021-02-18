793109bc9fd940c014b12ffa253ef7a93078c0a0
Haruki Nazawa <nazawaharuki@gmail.com>
2021-02-18 14:51:31 +0900

    サイズを全て相対値に変更

--- components/atoms/pad.tsx
+++ components/atoms/pad.tsx
@@ -1,14 +1,14 @@
 import React from "react";
-import { useDropzone } from "react-dropzone";
+import { FileRejection, useDropzone } from "react-dropzone";
+
+const ACCEPT_AUDIO_TYPES = ["audio/aac", "audio/mpeg", "audio/ogg", "audio/x-m4a", "audio/x-wav", "audio/x-aiff", "audio/x-flac"];
 
 export type PadProps = {
-  className?: string;
-  fileDraggable: boolean;
   children: React.ReactNode;
-  onDropFile: (file: any) => void;
+  onDropFile: (acceptedFile: File) => void;
   onMouseDown: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
   onMouseUp: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void
-}
+} & JSX.IntrinsicElements["div"];
 
 const PadButton = (props: PadProps) => {
   return (
@@ -23,21 +23,22 @@ const PadButton = (props: PadProps) => {
 }
 
 export default function Pad(props: PadProps) {
-  if (!props.fileDraggable) {
-    return (
-      <div className="w-24 h-24">
-        <PadButton {...props} />
-      </div>
-    );
-  }
+  const onDropped = (files: File[], fileRejections: FileRejection[]) => {
+    if (fileRejections.length > 0) {
+      return console.error("file is rejected.");
+    }
+    props.onDropFile(files[0]);
+  };
 
   const {getRootProps, getInputProps, isDragActive} = useDropzone({
-    onDrop: () => props.onDropFile,
+    onDrop: onDropped,
+    accept: ACCEPT_AUDIO_TYPES,
     noClick: true,
+    maxFiles: 1,
   });
 
   return (
-    <div {...getRootProps({ className: `w-24 h-24 bg-white ${props.className}` })}>
+    <div {...getRootProps({ className: `bg-white shadow-md ${props.className}` })}>
       <input {...getInputProps()} className="opacity-0" />
       {
         isDragActive ? 
