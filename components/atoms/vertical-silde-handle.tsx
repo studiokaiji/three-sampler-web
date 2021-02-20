0c6572aabe5ed54f46e4d221af60f59eca6dc1f9
Haruki Nazawa <nazawaharuki@gmail.com>
2021-02-20 11:19:21 +0900

    VerticalSlideHandleにリネーム

--- components/atoms/vertical-silde-handle.tsx
+++ components/atoms/vertical-silde-handle.tsx
@@ -4,7 +4,7 @@ type VerticalSlideBarProps = {
   barBgColor: string,
 } & JSX.IntrinsicElements["div"];
 
-export default function VerticalSlideBar(props: VerticalSlideBarProps) {
+export default function VerticalSlideHandle(props: VerticalSlideBarProps) {
   return (
     <div {...props}>
       <div className={props.barBgColor + " rounded-full w-2 h-2 -mt-2"} />
